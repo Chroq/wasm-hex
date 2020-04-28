86e76922da7e210fbd4e97f93c7f36dc2081063f
Christophe Lecroq <christophe.lecroq@gmail.com>
2020-04-28 22:00:20 +0200

    Add the bottom of the board

---
+++ www/components/boards/BottomBoard.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+const FIRST_PLAYER_COLOR = "#EEE";
+const SECOND_PLAYER_COLOR = "#111";
+
+const BottomBoard = (props) => {
+  return (
+      <svg width="94%" viewBox="0 0 174.767 104.46" {...props}>
+        <g>
+          <path
+            fill={FIRST_PLAYER_COLOR}
+            d="M.781 2.23c2.582-4.473 0 0 116.603-1.962l-30 51.962L.78 2.23z"
+          />
+          <path
+            fill={FIRST_PLAYER_COLOR}
+            d="M173.986 102.23c-2.582 4.473 0 0-116.602 1.962l30-51.962 86.602 50z"
+          />
+          <path
+            fill={SECOND_PLAYER_COLOR}
+            d="M.781 2.23c-2.582 4.473 0 0 56.603 101.962l30-51.962L.78 2.23z"
+          />
+          <path
+            fill={SECOND_PLAYER_COLOR}
+            d="M173.986 102.23c2.583-4.473 0 0-56.602-101.962l-30 51.962 86.602 50z"
+          />
+        </g>
+      </svg>
+  );
+};
+
+export default BottomBoard;
--- www/index.js
+++ www/index.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as wasm from "wasm-hex";
+import BottomBoard from "./components/boards/BottomBoard";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "#222222",
+        display: "flex",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          padding: "2%",
+          height: "80%",
+          width: "80%"
+        }}
+      >
+        <BottomBoard />
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
-wasm.greet();
