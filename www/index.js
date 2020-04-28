bbda3e2b3135a1002bc21b540911f9d00c62f9ef
Christophe Lecroq <christophe.lecroq@gmail.com>
2020-04-28 14:00:35 +0200

    Implement basic App

--- www/index.js
+++ www/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as wasm from "wasm-hex";
 
-ReactDOM.render(<h1>Hi there</h1>, document.getElementById("root"));
+const App = () => {
+  return <div></div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 wasm.greet();
