15c9271bd812a78894e0c043167d8d7a50d5613c
Christophe Lecroq <christophe.lecroq@gmail.com>
2020-04-28 13:55:02 +0200

    Install React and Babel

--- www/index.js
+++ www/index.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import * as wasm from "wasm-hex";
 
+ReactDOM.render(<h1>Hi there</h1>, document.getElementById("root"));
 wasm.greet();
