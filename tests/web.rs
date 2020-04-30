658350674979fda24add3267cca8288d0b1660fa
Christophe Lecroq <christophe.lecroq@gmail.com>
2020-05-01 00:34:15 +0200

    test: Check of cells generation

--- tests/web.rs
+++ tests/web.rs
@@ -1,13 +1,11 @@
 //! Test suite for the Web and headless browsers.
-
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+extern crate wasm_hex;
+
+pub mod board;
+
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
-
-#[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
-}
